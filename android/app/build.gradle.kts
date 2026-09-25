82f7f87ec3208e1c95896ed16c30f35c873a3827
Abrar Jawad <abrar13113@gmail.com>
2026-09-25 12:25:11 +0600

    Bump Android version to 1.1.0-branch2 for v1.1-android release
    versionName was still "0.1.0-skeleton" despite Segment 19's Branch 2
    morphology port and Segment 16-18 work already being on main.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         // hard library requirement.
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-skeleton"
+        versionCode = 2
+        versionName = "1.1.0-branch2"
     }
 
     buildTypes {
